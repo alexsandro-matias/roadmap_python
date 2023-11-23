54097236b2d41566529930d07c765330dae60e84
alexsandro-matias <matiasalexsandro@gmail.com>
2023-11-23 11:20:22 -0300

    iniciando lógica de programação em python

--- README.md
+++
@@ -1,5 +0,0 @@
-# Configuração do VS Code para os diferentes Sistemas Operacionais:
-
-- Ubuntu 22
-- Windows 11
- 
--- instalacao_simplificada_python.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install git curl build-essential -y
-sudo apt install gcc make default-libmysqlclient-dev libssl-dev -y
-sudo apt install python3.10-full python3.10-dev -y
